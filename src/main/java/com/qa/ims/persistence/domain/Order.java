4a82f4d63535a42b638a9078bddac583160c1d76
Christian Jardine <christian.s.jardine@gmail.com>
2022-07-26 12:54:07 +0100

    Created order system of controller, DAO , order classes and updated the IMS to call these order classes

---
+++ src/main/java/com/qa/ims/persistence/domain/Order.java
@@ -0,0 +1,83 @@
+package com.qa.ims.persistence.domain;
+
+import java.sql.Date;
+import java.util.Objects;
+
+public class Order {
+	
+	private Long orderID;
+	private Date orderDate;
+	private Long customerID;
+	private Long productID;
+	
+	public Order(Long customerid, Long productid) {
+		this.customerID = customerid;
+		this.productID = productid;
+	}
+	
+	public Order(Long id, Date orderDate, Long customerid, Long productid) {
+		this.setOrderID(id);
+		this.setOrderDate(orderDate);
+		this.setCustomerID(customerid);
+		this.setProductID(productid);
+		
+	}
+
+	public Long getOrderID() {
+		return orderID;
+	}
+
+	public void setOrderID(Long orderID) {
+		this.orderID = orderID;
+	}
+
+	public Date getOrderDate() {
+		return orderDate;
+	}
+
+	public void setOrderDate(Date orderDate) {
+		this.orderDate = orderDate;
+	}
+
+	public Long getCustomerID() {
+		return customerID;
+	}
+
+	public void setCustomerID(Long customerID) {
+		this.customerID = customerID;
+	}
+
+	public Long getProductID() {
+		return productID;
+	}
+
+	public void setProductID(Long productID) {
+		this.productID = productID;
+	}
+
+	@Override
+	public String toString() {
+		return "Order [orderID=" + orderID + ", orderDate=" + orderDate + ", customerID=" + customerID + ", productID="
+				+ productID + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(customerID, orderDate, orderID, productID);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		return Objects.equals(customerID, other.customerID) && Objects.equals(orderDate, other.orderDate)
+				&& Objects.equals(orderID, other.orderID) && Objects.equals(productID, other.productID);
+	}
+	
+	
+}
