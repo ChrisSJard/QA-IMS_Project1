77ab72f662973a70491a2f642bd0a25887297299
ChrisJardingWindows <christian.s.jardine@gmail.com>
2022-07-27 11:51:32 +0100

    Update to sql scheme

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -1,5 +1,5 @@
-#drop schema ims;
-
+CREATE database IF NOT EXISTS ims;
+drop schema ims;
 CREATE SCHEMA IF NOT EXISTS `ims`;
 
 USE `ims` ;
@@ -9,4 +9,29 @@ CREATE TABLE IF NOT EXISTS `ims`.`customers` (
     `first_name` VARCHAR(40) DEFAULT NULL,
     `surname` VARCHAR(40) DEFAULT NULL,
     PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `ims`.`products` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(40) DEFAULT NULL,
+    `description` VARCHAR(120) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `ims`.`orders` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `customer_id` INT(11),
+    `order_date` Date,
+    `required_date` Date,
+    PRIMARY KEY (`id`),
+    foreign key (`customer_id`) references customers(id)
+);
+
+CREATE TABLE IF NOT EXISTS `ims`.`order_products` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `product_id` INT(11),
+    `order_id` INT(11),
+    PRIMARY KEY (`id`),
+    foreign key (`product_id`) references products(id),
+    foreign key (`order_id`) references orders(id)
 );
